963f30439ffcd13064a344ae03963dac0aa406f1
strahinjapraska <strahinja.praska6@gmail.com>
2026-02-15 19:11:51 +0100

    OAEP

--- src/lib.rs
+++ src/lib.rs
@@ -40,6 +40,73 @@ mod tests {
         let (pk, sk) = rsa_oaep::keygen(); 
         let m = b"67"; 
         let l = b"67";
-        rsa_oaep::enc(&pk, m, l);
+        let c = rsa_oaep::enc(&pk, m, l);
+        let m_prime = rsa_oaep::dec(&sk, &c, l);
+        assert_eq!(m_prime, m.to_vec());
     }
+
+    fn dummy_hash()-> [u8; 32]{
+        [0xAA; 32]
+    }
+
+    fn build_db(ps: &[u8], l_hash: &[u8], m: &[u8], delimiter: u8) -> Vec<u8>{
+        let mut db = Vec::new();
+        db.extend_from_slice(l_hash);
+        db.extend_from_slice(ps);
+        db.push(delimiter);
+        db.extend_from_slice(m);
+        return db;
+    }
+
+    #[test] 
+    fn db_valid_test_empty_ps(){
+        let msg = b"msg"; 
+        let db = build_db(&[], &dummy_hash(), msg, 0x01); 
+        let (ok, idx) = rsa_oaep::is_db_valid(&db, &dummy_hash(), 0);
+        assert_eq!(ok.unwrap_u8(), 1u8);
+        assert_eq!(idx, 33);
+    }
+
+    #[test]
+    fn db_valid_test_1(){
+        let msg = b"msg"; 
+        let ps = [0u8; 10]; 
+        let db = build_db(&ps, &dummy_hash(), msg, 0x01); 
+        let (ok, idx) = rsa_oaep::is_db_valid(&db, &dummy_hash(), 0);
+        assert_eq!(ok.unwrap_u8(), 1u8);
+        assert_eq!(idx, 43);
+    }
+
+    #[test]
+    fn db_valid_test_2(){
+        let msg = b"msg"; 
+        let ps = [0u8; 10]; 
+        let mut db = build_db(&ps, &dummy_hash(), msg, 0x02); 
+        let (ok, _) = rsa_oaep::is_db_valid(&db, &dummy_hash(), 0); 
+        assert_eq!(ok.unwrap_u8(), 0u8);
+    }
+
+    #[test] 
+    fn db_valid_test_3(){
+        let msg = b"msg"; 
+        let mut ps = vec![0u8;5]; 
+        ps.push(0x02); 
+        ps.extend_from_slice(&[0u8; 5]);
+
+        let mut db = build_db(&ps, &dummy_hash(), msg, 0x01); 
+        db[0] = 0xFF; 
+        let (ok, _) = rsa_oaep::is_db_valid(&db, &dummy_hash(), 0); 
+        assert_eq!(ok.unwrap_u8(), 0u8);
+
+    }
+
+    #[test]
+    fn db_valid_test_4(){
+        let msg = [0x01, 0x02, 0x03]; 
+        let ps = vec![0u8;5]; 
+        let db = build_db(&ps, &dummy_hash(), &msg, 0x01);  
+        let (ok, idx) = rsa_oaep::is_db_valid(&db, &dummy_hash(), 0);
+        assert_eq!(ok.unwrap_u8(), 1u8);
+        assert_eq!(idx, 32+5+1);
+    } 
 }
--- src/rsa_oaep.rs
+++ src/rsa_oaep.rs
@@ -1,6 +1,10 @@
+use std::ops::BitAnd;
+
 use crate::{rsa}; 
 use crate::common::*;  
 use crypto_bigint::U3072;
+use crypto_bigint::rand_core::le;
+use crypto_bigint::subtle::{Choice, ConditionallySelectable, ConstantTimeEq};
 use sha2::{Digest, Sha256};
 use rand::RngCore;
 
@@ -21,12 +25,29 @@ pub fn enc(pk: &rsa::RSAPublicKey, m: &[u8], l: &[u8]) -> [u8; 384]{
     return c.to_be_bytes(); 
 }
 
-pub fn dec(sk: &rsa::RSAPrivateKey, c: &[u8]) -> Vec<u8>{
-    unimplemented!();
+pub fn dec(sk: &rsa::RSAPrivateKey, c: &[u8], l: &[u8]) -> Vec<u8>{
+    assert!((l.len() as u64) <= 2u64.pow(61) - 1);  
+    assert!(c.len() == K);
+    assert!(K > 2*H_LEN + 2);
+
+    // NOTE: When catching error from plain RSA, handle it properly. 
+    let em = rsa::dec(sk, &U3072::from_be_slice(c)).to_be_bytes(); 
+    let m = eme_oaep_decode(&em, l); 
+
+    return m; 
 }
 
 pub fn eme_oaep_encode(m: &[u8], l: &[u8]) -> [u8; K]{
-    let l_hash = Sha256::digest(l); 
+    let l_hash = if l.len() == 0{
+        [
+            0xe3, 0xb0, 0xc4, 0x42, 0x98, 0xfc, 0x1c, 0x14,
+            0x9a, 0xfb, 0xf4, 0xc8, 0x99, 0x6f, 0xb9, 0x24,
+            0x27, 0xae, 0x41, 0xe4, 0x64, 0x9b, 0x93, 0x4c,
+            0xa4, 0x95, 0x99, 0x1b, 0x78, 0x52, 0xb8, 0x55,
+        ]
+    }else{
+        Sha256::digest(l).into()
+    }; 
 
     let ps = vec![0u8; K - m.len() - 2*H_LEN - 2];
 
@@ -60,3 +81,80 @@ pub fn eme_oaep_encode(m: &[u8], l: &[u8]) -> [u8; K]{
     return em; 
 }
 
+pub fn eme_oaep_decode(em: &[u8], l: &[u8]) -> Vec<u8>{
+     let l_hash = if l.len() == 0{
+        [
+            0xe3, 0xb0, 0xc4, 0x42, 0x98, 0xfc, 0x1c, 0x14,
+            0x9a, 0xfb, 0xf4, 0xc8, 0x99, 0x6f, 0xb9, 0x24,
+            0x27, 0xae, 0x41, 0xe4, 0x64, 0x9b, 0x93, 0x4c,
+            0xa4, 0x95, 0x99, 0x1b, 0x78, 0x52, 0xb8, 0x55,
+        ]
+    }else{
+        Sha256::digest(l).into()
+    }; 
+    
+    let y = em[0];
+    let masked_seed = &em[1..1+H_LEN];
+    let masked_db = &em[1+H_LEN..]; 
+
+    let seed_mask = mgf1(masked_db, H_LEN);
+    let mut seed = [0u8; H_LEN]; 
+    for i in 0..seed_mask.len(){
+        seed[i] = masked_seed[i] ^ seed_mask[i];
+    }
+
+    let db_mask = mgf1(&seed, K - H_LEN - 1);
+    let mut db = [0u8; K - H_LEN - 1]; 
+    for i in 0..db_mask.len(){
+        db[i] = masked_db[i] ^ db_mask[i];
+    }
+
+    let (ok, msg_idx) = is_db_valid(&db, &l_hash, y); 
+    if ok.unwrap_u8() == 0u8 {
+        panic!("Decryption error");
+    } 
+
+    let m = &db[msg_idx..]; 
+
+    return m.to_vec(); 
+
+
+}
+
+pub(crate) fn is_db_valid(db: &[u8], l_hash : &[u8], y: u8) -> (Choice, usize){
+    let is_y_zero = y.ct_eq(&0u8);
+
+    let l_hash_prime = &db[0..H_LEN];
+    let is_l_hash_valid = l_hash_prime.ct_eq(l_hash); 
+
+    let rest = &db[H_LEN..];  
+
+    let mut found = Choice::from(0u8);
+    let mut bad_event = Choice::from(0u8);
+
+    let mut msg_idx = rest.len();
+     
+    for i in 0..rest.len(){
+        let b = rest[i]; 
+        let is_zero = b.ct_eq(&0u8); 
+        let is_one = b.ct_eq(&0x01u8); 
+        
+        bad_event |= (!found) & !(is_zero | is_one);  
+
+        let first_occurence = (!found)& is_one; 
+
+        // mutliplexer 
+        let c = first_occurence.unwrap_u8() as usize;
+        let mask = 0usize.wrapping_sub(c);
+        msg_idx = (msg_idx & !mask) | ((i+1) & mask);
+        
+        found |= is_one;
+    }
+    
+    let mut ok = is_y_zero; 
+    ok = ok.bitand(is_l_hash_valid); 
+    ok = ok.bitand(found); 
+    ok = ok.bitand(!bad_event);
+    
+    return (ok, H_LEN + msg_idx);
+}
